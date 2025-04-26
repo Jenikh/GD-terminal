3f450250c2440dddb07ad98a6804ad8879e3e7b3
Jenikh <164548559+Jenikh@users.noreply.github.com>
2025-04-26 10:15:04 +0200

    Add files via upload

---
+++ template.py
@@ -0,0 +1,6 @@
+# Want to update the template or help run "py main.py gentemplate" with wifi
+# obj = {[1,0]: "block", [5,0]: "spike",[6,0]:"end"}
+# ^^^^^^^^^^^^^^^^^^^^^^
+# This is the main part of the level
+# If you want to change the level, change the obj
+# If you want to try this rename this file to level.py and uncoment the obj
